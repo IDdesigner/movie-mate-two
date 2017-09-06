c510d6c03d997adb2c91222707fe4129829cae39
Rye Crowen <ryecrowendesign@mac.com>
2017-09-06 00:08:01 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+'use strict';
+// create an API server
+const Restify = require('restify');
+const server = Restify.createServer({
+	name: 'MovieMate2'
+});
+const tmdb = require('./tmdb');
+const PORT = process.env.PORT || 3000;
+
+server.use(Restify.jsonp());
+server.use(Restify.bodyParser());
+server.use((req,res,next) => f.verifySignature(req,res,next));
+
+// Tokens
+const config = require('./config');
+
+// FBeamer
+const FBeamer = require('./fbeamer');
+const f = new FBeamer(config.FB);
+
+// Your Bot here
+
+// Register the webhooks
+server.get('/', (req, res, next) => {
+	f.registerHook(req, res);
+	return next();
+});
+
+// Receive all incoming messages
+server.post('/', (req, res, next) => {
+	f.incoming(req, res, msg => {
+		// Process messages
+		const {
+			message,
+			sender
+		} = msg;
+
+
+		if(message.text && message.nlp.entities) {
+			tmdb(message.nlp.entities)
+				.then(response => {
+					f.txt(sender, response.txt);
+					if(response.img) {
+						f.img(sender, response.img);
+					}
+				})
+				.catch(error => {
+					console.log(error);
+					f.txt(sender, 'My servers are acting up. Do check back later...');
+				});
+			// If a text message is received
+			f.txt(sender, `You just said ${message.text}`);
+		}
+	});
+	return next();
+});
+
+// Subscribe
+f.subscribe();
+
+server.listen(PORT, () => console.log(`myBot running on port ${PORT}`));
